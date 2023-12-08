7974d0e3be355bd25c8c85559381a22ebb2db380
fradjmag38 <fradjdorbez2001@gmail.com>
2023-12-08 06:53:23 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Navbar from './components/Navbar.svelte';
+	import Map from './components/Map.svelte';
+  import sidebar from './components/sidebar.svelte';
+    import Sidebar from './components/sidebar.svelte';
+</script>
+
+<div class="app">
+  <Navbar/>
+	<Map/>
+  <Sidebar/>
+</div>
+
+<style>
+  .app {
+    text-align: center;
+  }
+</style>
---
+++ src/components/sidebar.svelte
@@ -0,0 +1,18 @@
+<div class="heading">
+    <h1>.</h1>
+  </div>
+  
+  <style>
+    .heading {
+      margin: 0;
+      padding: 0px;
+      background-color: black;
+      position: 'fixed';
+      color: white;
+    }
+  
+    .heading > h1 {
+      padding: 20px;
+      margin: 0;
+    }
+  </style>
